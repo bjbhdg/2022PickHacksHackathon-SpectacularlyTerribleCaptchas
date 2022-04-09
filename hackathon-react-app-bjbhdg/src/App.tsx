11cef08620610dbba595be0b51010158047b963c
bjbhdg <bjbhdg@umsystem.edu>
2022-04-09 03:36:39 -0500

    GUI component under development
    I decided to shift away from "pop-ups" randomly appearing on screen to simply clicking on the extension on the taskbar. I only have the welcome screen created, but it can be subject to change.

--- hackathon-react-app-bjbhdg/src/App.tsx
+++ hackathon-react-app-bjbhdg/src/App.tsx
@@ -1,23 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
+import CSS from "csstype"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit nothing and don't save to not reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mainContainer: CSS.Properties = {
+  height: "auto",
+  width: "auto",
+  marginLeft: "10px",
+  marginRight: "10px",
+}
+
+const title: CSS.Properties = {
+  marginTop: "10px",
+  marginBottom: "10px",
+  textAlign: "center",
+  fontFamily: "arial",
+  fontSize: "16px",
+  fontWeight: "bold",
+}
+
+const normalText: CSS.Properties = {
+  marginTop: "10px",
+  marginBottom: "10px",
+  textAlign: "center",
+  fontFamily: "arial",
+  fontSize: "12px",
+}
+
+const button: CSS.Properties = {
+  marginTop: "10px",
+  marginBottom: "10px",
+  backgroundColor: "blue",
+  border: "none",
+  textAlign: "center",
+  margin: "auto",
+  display: "flex",
+  color: "white",
+  fontFamily: "arial",
+  fontSize: "12px",
+}
+
+interface IProps {}
+
+interface IState {
+  welcomeOpen: boolean,
+  inputPhoneOpen: boolean,
+  pictureCaptchaOpen: boolean
+}
+
+class App extends Component<IProps, IState>{
+  constructor(props: IProps) {
+    super(props)
+    this.state = {
+      welcomeOpen: true,
+      inputPhoneOpen: false,
+      pictureCaptchaOpen: false,
+    }
+  }
+
+  welcomeMenu(): JSX.Element {
+    return(
+      <div style={mainContainer} className="Welcome Menu">
+        <header style={title}>
+          Welcome to the Laughly Bad "Captcha-Like" Task Extension!
+        </header>
+        <div style={normalText}>
+          Press the Button Below to Begin... Have Fun!
+        </div>
+        <button style={button} type="button" onClick={() => this.setState({ welcomeOpen: false })}>
+          Click Here
+        </button>
+      </div>
+    );
+  }
+
+  render(): JSX.Element {
+    return (
+      <div>
+        {this.state.welcomeOpen ? this.welcomeMenu() : null}
+      </div>
+    );
+  }
 }
 
 export default App;
