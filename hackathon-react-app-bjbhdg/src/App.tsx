d11ee2a7130bd1c4289ebcce84ce97510766bdbe
bjbhdg <bjbhdg@umsystem.edu>
2022-04-10 02:53:21 -0500

    A Few Finishing Touches
    For the captcha page, I combined both an image and a checkbox in one grid element to give the impression of selecting that image. I also made some UI changes, finalizing component sizes/colors. In addition, some documentation has been made.

--- hackathon-react-app-bjbhdg/src/App.tsx
+++ hackathon-react-app-bjbhdg/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import styles from './App.module.css'
+import styles from './App.module.css' // CSS styles located here.
 
 interface IProps {}
 
 interface IState {
-  allowErrorPopUp: boolean
+  allowAlertPopUp: boolean
   welcomeOpen: boolean
   inputPhoneOpen: boolean
   userPhoneNumber: number
@@ -12,11 +12,22 @@ interface IState {
 }
 
 function outputPhoneFormat(phoneNum: number): string {
+  // Cuts off the trailing seven numbers in the phone number (*xxx*) xxx-xxxx.
   const firstThreeDigits = Math.floor(phoneNum / 10000000)
+
+  // Retrieves these (surrounded by '*') numbers in a phone number (xxx) *xxx*-xxxx
   const nextThreeDigits = Math.floor((phoneNum - (firstThreeDigits * 10000000)) / 10000)
-  const nearlyLastThreeDigits = Math.floor((phoneNum - (firstThreeDigits * 10000000 + nextThreeDigits * 10000)) / 10)
+
+  // Retrieves these (surrounded by '*') three digits: (xxx) xxx-*xxx*x
+  const nearlyLastThreeDigits = Math.floor((phoneNum - 
+    (firstThreeDigits * 10000000 + nextThreeDigits * 10000)) / 10)
+
+  // Last digit of the phone number is retrieved, this is done because .toLocaleString()
+  // below will forcibly place a comma in a number of greater than 3 digits.
   const lastDigit = phoneNum % 10
 
+  // Formats the number as follows:
+  // (firstThreeDigits) nextThreeDigits-nearlyLastThreeDigitslastDigit [(xxx) xxx-xxxx]
   return `(${
     firstThreeDigits.toLocaleString(undefined, {minimumIntegerDigits: 3,})}) ${
     nextThreeDigits.toLocaleString(undefined, {minimumIntegerDigits: 3},)}-${
@@ -27,7 +38,7 @@ class App extends React.Component<IProps, IState>{
   constructor(props: IProps) {
     super(props)
     this.state = {
-      allowErrorPopUp: false,
+      allowAlertPopUp: false,
       welcomeOpen: true,
       inputPhoneOpen: false,
       userPhoneNumber: 0,
@@ -35,8 +46,9 @@ class App extends React.Component<IProps, IState>{
     }
   }
 
+  // Called every time the blue button at the bottom of each page is pressed.
   handlePageChange(): void {
-    // Close the welcoming screen.
+    // Close the welcoming screen, if currently on it.
     if(this.state.welcomeOpen) {
       this.setState({welcomeOpen: false})
     }
@@ -45,7 +57,7 @@ class App extends React.Component<IProps, IState>{
     if(!this.state.inputPhoneOpen && !this.state.captchaOpen) {
       switch(Math.floor(Math.random() * 2)) {
         case 0:
-          this.setState({ inputPhoneOpen: true, captchaOpen: false, userPhoneNumber: 0 })
+          this.setState({ inputPhoneOpen: true, captchaOpen: false })
           break
         case 1:
           this.setState({ inputPhoneOpen: false, captchaOpen: true })
@@ -57,34 +69,73 @@ class App extends React.Component<IProps, IState>{
     } else if(this.state.captchaOpen) {
       this.setState({ inputPhoneOpen: true, captchaOpen: false })
     }
+
+    // Under no circumstances will the user-entered phone number will be kept between page changes,
+    // as well as alert message.
+    this.setState({ userPhoneNumber: 0, allowAlertPopUp: false })
   }
 
+  // Whenever the user attempts to submit their input, an alert is displayed on top of
+  // the page.
   randomAlertText(): string {
-    const { inputPhoneOpen } = this.state
+    const { inputPhoneOpen, captchaOpen } = this.state
 
-    let alert = ""
+    let alert: string = ""
 
+    // Alerts specifically for the phone number entering page.
     if(inputPhoneOpen) {
       const phoneInputAlertList = [
         "Your e-mail seems to be incorrect, try again.",
         "We're sorry, but your e-mail already has an account associated with it.",
         "Try entering your phone number again (it should be simple!).",
         "Connection timed out...",
-        "Just try again, you'll get it soon!"
+        "Just try again, you'll get it soon!",
+        "Did you forget your phone number? You better not have...",
+        "You're really using that email? In 2022???",
+        "Just remember, if you're tired of this, press the button that says \"Other\"",
+        "You'll get this, we believe in you!",
+        "You're lucky we're not grabbing your IP!",
+        "Think long and hard. Remember, you're not logging in, you're creating an account!",
+        "Look at all that hard work you did! Unfortunately your phone number is already claimed."
       ]
       
       alert = phoneInputAlertList[Math.floor(Math.random() * phoneInputAlertList.length)]
+    }
+    // Alerts specifically for the captcha page. 
+    else if(captchaOpen) {
+      const captchaAlertList = [
+        "Aren't cats just the greatest?",
+        "Dogs are pretty cool, right?",
+        "Cats are better, change my mind.",
+        "Come on, it's a captcha! What are you? A robot?",
+        "The black/white cat's name is Boots!",
+        "The dark gray/white, fat cat's name is Socks!",
+        "The black/white, pig-like dog's name is Lucy!",
+        "The big, yellow, greedy dog's name is Bella!",
+        "The small, brown dog, mid-jump, is named Poppy!",
+        "We're not sure what the bowl/chair cat's name is...",
+        "Are you starting to realize?... You know what, nevermind.",
+        "To continue, say \"I prefer cats over dogs\" out loud.",
+        "How are you liking this so far? Entertained???",
+        "Do you have any pets? We would love to know!",
+        "Doesn't image stretch make the world go 'round?"
+      ]
+
+      alert = captchaAlertList[Math.floor(Math.random() * captchaAlertList.length)]
     }
 
     return alert
   }
   
   authenticatePhoneNum(phoneNum: number): number {
-    const maxNum = 9999999999
-    let authenticatedPhoneNum = phoneNum
+    // The maximum theoretical phone number would be (999) 999-9999.
+    const maxNum: number = 9999999999
+    let authenticatedPhoneNum: number = phoneNum
 
+    // Overflows the phone number to zero.
     if(authenticatedPhoneNum > maxNum) {
       authenticatedPhoneNum = 0
+    // Underflows the phone number to the max phone number.
     } else if(authenticatedPhoneNum < 0) {
       authenticatedPhoneNum = maxNum
     }
@@ -92,13 +143,16 @@ class App extends React.Component<IProps, IState>{
     return authenticatedPhoneNum
   }
 
+  // Returns a random image from the directory 'hackathon-react-app-bjbhdg-public-pics'
+  // (the directory is navigated from 'index.html' in the public folder).
   randomImageSelector(): JSX.Element {
-    let fileDir = ""
+    let fileDir: string = ""
 
-    // There is currently no method of browser extensions having access to local files
-    // (specifically regarding the use of 'fs' and 'path'), so the random image selector
-    // is hardcoded below.
-    switch(Math.floor(Math.random() * 10)) {
+    // There is currently no method for firefox browser extensions to obtain access to
+    // local files without needing user input. Since the captcha needs to be filled with random
+    // images from the pics/ folder, I unfortunatetly must hardcode every directory to correctly
+    // output the image.
+    switch(Math.floor(Math.random() * 20)) {
       case 0:
         fileDir = "./pics/20220326_171921.jpg"
         break
@@ -134,63 +188,121 @@ class App extends React.Component<IProps, IState>{
         break
       case 11:
         fileDir = "./pics/IMG_20211122_205738-1.jpg"
+        break
+      case 12:
+        fileDir = "./pics/Resized_20211031_0131221.jpeg"
+        break
+      case 13:
+        fileDir = "./pics/Resized_20211031_135824.jpeg"
+        break
+      case 14:
+        fileDir = "./pics/Resized_20220306_163016.jpeg"
+        break
+      case 15:
+        fileDir = "./pics/Resized_20220219_1952291.jpeg"
+        break
+      case 16:
+        fileDir = "./pics/Resized_20220204_2129581.jpeg"
+        break
+      case 17:
+        fileDir = "./pics/Resized_20211207_1504381.jpeg"
+        break
+      case 18:
+        fileDir = "./pics/Resized_20210429_14393111.jpeg"
+        break
+      case 19:
+        fileDir = "./pics/Resized_20211212_125918.jpeg"
     }
 
+    // A JSX Element is returned of the image chosen above, then size of the image is forcibly
+    // made to be 125px by 125px, so some pictures will have HEAVY stretch.
     return(
-      <img src={fileDir} alt={fileDir} width= "100px" height= "100px"/>
+      <img src={fileDir} alt={fileDir} width="120px" height="120px"/>
     )
   }
 
+  // Uses a map function to render each cell in the 3x3 captcha grid.
+  tableEntries(): JSX.Element {
+    // A list of nine integers is used to represent each of the nine cells in the captcha grid.
+    const ids: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+    // Creates a cell for each of the nine total cells.
+    const dataEntries: JSX.Element[] = ids.map((cell) =>
+      <div className={styles.captchaImageCell}>
+        <input type="checkbox" key={cell}></input>
+        {this.randomImageSelector()}
+      </div>
+    )
+
+    // Encapsulates all of the previous cells.
+    return (
+      <div style={{ justifyContent: "center", marginBottom: "10px" }}
+        className={styles.captchaImageColumn}
+      >
+        {dataEntries}
+      </div>
+    )
+  }
+
+  // Welcoming screen for the browser extension (will only ever show up once).
   welcomeMenu(): JSX.Element {
     return(
       <div className={styles.mainContainer} id="Welcome Menu">
         <header className={styles.title}>
           Welcome to the Spectacularly Terrible Captchas!
         </header>
-        <div className={styles.normalText}>
-          Press the Button Below to Begin... Have Fun!
+        <div className={styles.normalText} id="User Instruction Prompt">
+          Press the Button Below to Begin (and Navigate Pages Later)... Have Fun!
         </div>
       </div>
     )
   }
 
+  // Contains the HTML for the phone number entering page.
   inputPhone(): JSX.Element {
     return(
-      <div className={styles.mainContainer} id="Input Phone Number">
-        <header className={styles.title}>
+      <div className={styles.mainContainer} id="Input Phone Number Page">
+        <header className={styles.title} id="Phone Number Title">
           Welcome!
           <p className={styles.normalText}>
             To create you account, please input your email and phone number:
           </p>
         </header>
         <div>
-          <div className={styles.alertText}>
-            {this.state.allowErrorPopUp ? this.randomAlertText() : null}
-            {this.state.allowErrorPopUp
-              ? clearTimeout(setTimeout(() => {this.setState({allowErrorPopUp: false})}, 1500))
-              : null
-            }
+          <div className={styles.alertText} id="Phone Input Alert Space">
+            {/* Alerts will only show up if allowed. */}
+            {this.state.allowAlertPopUp ? this.randomAlertText() : null}
           </div>
-          <div className={styles.normalText}>
+          <div className={styles.normalText} id="Email Input Field">
             <label>Email:</label>
-            <input type="email" id="userEMail" /><br/>
+            {/* Every time a user interacts with the data inputs in some way, the alert
+              * on top of the page will be hidden (if previously visible).*/}
+            <input type="email" onClick={() => 
+              {this.setState({ allowAlertPopUp: false })}}
+            /><br/>
           </div>
-          <div className={styles.normalText}>
+          <div className={styles.normalText} id="Phone Number Input Field">
             <label>Phone Number:</label>
             <input readOnly={true} type="string"
-              value={outputPhoneFormat((this.state.userPhoneNumber))} />
+              onClick={() => {this.setState({ allowAlertPopUp: false })}}
+              value={outputPhoneFormat((this.state.userPhoneNumber))}
+            />
             <button type="button" style={{ width: "auto" }} onClick={
-              () => this.setState({ userPhoneNumber: this.authenticatePhoneNum(this.state.userPhoneNumber + 1 )})}>
+              () => this.setState({ allowAlertPopUp: false, userPhoneNumber:
+              this.authenticatePhoneNum(this.state.userPhoneNumber + 1)})}
+            >
               +
             </button>
             <button type="button" style={{ width: "auto" }} onClick={
-              () => this.setState({ userPhoneNumber: this.authenticatePhoneNum(this.state.userPhoneNumber - 1 )})}>
+              () => this.setState({ allowAlertPopUp: false, userPhoneNumber:
+              this.authenticatePhoneNum(this.state.userPhoneNumber - 1 )})}
+            >
               -
             </button><br/>
           </div>
-          <div style={{ textAlign: "center" }}>
+          <div style={{ textAlign: "center" }} id="Submit Field">
             <button type="button" style={{ marginBottom: "10px" }} onClick={
-              () => this.setState({ allowErrorPopUp: true })
+              () => this.setState({ allowAlertPopUp: true })
             }>
               Submit
             </button>
@@ -200,45 +312,38 @@ class App extends React.Component<IProps, IState>{
     )
   }
 
+  // Contains the HTML code for the "Captcha" page.
   captchaMenu(): JSX.Element {
     return (
       <div className={styles.mainContainer} id="Captcha Menu">
-        <header className={styles.title}>
+        <header className={styles.title} id="Captcha Title">
           Captcha Time!
           <p className={styles.normalText}>
             You know what to do...
           </p>
         </header>
-        <div className={styles.normalText}>
-          Select Pictures of {(Math.floor(Math.random() * 2)) ? "Cats" : "Dogs"}
+        <div className={styles.alertText} id="Captcha Alert Space">
+          {this.state.allowAlertPopUp ? this.randomAlertText() : null}
+        </div>
+        <div className={styles.normalText} id="Captcha Task Description">
+          {/* Will randomly prompt the user to select pictures of cats or dogs.*/}
+          Select Pictures of <b> {(Math.floor(Math.random() * 2)) ? "Cats" : "Dogs"}
+          </b> Using the Checkboxes
         </div> 
-        <div style={{ justifyContent: "center", marginBottom: "10px" }} className={styles.captchaImageColumn}>
-          <button className={styles.captchaImageCell}>
-            {this.randomImageSelector()}
+        {/* A 3x3 grid of 9 random images will be placed here. */}
+        {this.tableEntries()}
+        <div style={{ textAlign: "center" }} id="Submission/Reset Buttons">
+          {/* The setState call forcibly reshuffles each image in the captcha grid. */}
+          <button type="button" style={{ marginRight: "5px", marginBottom: "10px"}} onClick={
+              () => this.setState({ allowAlertPopUp: false })}
+          >
+            Reset
           </button>
-          <button className={styles.captchaImageCell}>
-            {this.randomImageSelector()}
-          </button>
-          <button className={styles.captchaImageCell}>
-            {this.randomImageSelector()}
-          </button>
-          <button className={styles.captchaImageCell}>
-            {this.randomImageSelector()}
-          </button>
-          <button className={styles.captchaImageCell}>
-            {this.randomImageSelector()}
-          </button>
-          <button className={styles.captchaImageCell}>
-            {this.randomImageSelector()}
-          </button>
-          <button className={styles.captchaImageCell}>
-            {this.randomImageSelector()}
-          </button>
-          <button className={styles.captchaImageCell}>
-            {this.randomImageSelector()}
-          </button>
-          <button className={styles.captchaImageCell}>
-            {this.randomImageSelector()}
+          {/* The captcha page is reset, along with allowing alerts to be displayed.*/}
+          <button type="button" style={{ marginLeft: "5px", marginBottom: "10px" }} onClick={
+              () => this.setState({ allowAlertPopUp: true })}
+          >
+            Submit
           </button>
         </div>
       </div>
@@ -251,8 +356,10 @@ class App extends React.Component<IProps, IState>{
         {this.state.welcomeOpen ? this.welcomeMenu() : null}
         {this.state.inputPhoneOpen ? this.inputPhone() : null}
         {this.state.captchaOpen ? this.captchaMenu() : null}
-        <button className={styles.button} type="button" onClick={() => this.handlePageChange()}>
-          {this.state.welcomeOpen ? "Click Here" : "Next..."}
+        <button className={styles.button} type="button" id="Page Navigation Button"
+          onClick={() => this.handlePageChange()}
+        >
+          {this.state.welcomeOpen ? "Click Here" : "Other"}
         </button>
       </div>
     )
